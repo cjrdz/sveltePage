f4adb8520474eaf3efc224ca84b6a32f58974308
cjrdz <jonathanrdzdev@gmail.com>
2024-10-23 14:40:38 -0600

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<h1>Welcome to SvelteKit</h1>
+<a href="/about">About my site</a>
+<br>
+<a href="/blog/hello-world">Hello world</a>
---
+++ src/routes/blog/[slug]/+page.ts
@@ -0,0 +1,14 @@
+import { error } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = ({ params }) => {
+	if (params.slug === 'hello-world') {
+		return {
+			title: 'Hello world!',
+			content: 'Welcome to our blog. Lorem ipsum dolor sit amet... ',
+			link: '<a href="/">Home</a>'
+		};
+	}
+
+	error(404, 'Not found');
+};
