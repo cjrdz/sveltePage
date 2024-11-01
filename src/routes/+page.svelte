69c6bfc11d792c6b6cf4078126da8daff168d464
cjrdz <jonathanrdzdev@gmail.com>
2024-11-01 15:23:48 -0600

    Skeleton Added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
+<!-- <h1>Welcome to SvelteKit tutorials</h1>
 <a href="/about">About my site</a>
 <br>
-<a href="/blog/hello-world">Hello world</a>
+<a href="/blog/hello-world">Hello world</a> -->
+<header class="card-header">
+    <h1 class="h1">Rust tutorials</h1>
+</header>
+<hr class="!border-t-8" />
+<body>
+	<div class="card">
+		<section class="p-4">Lesson 1</section>
+        <section class="p-4">Lesson 2</section>
+        <section class="p-4">Lesson 3</section>
+	</div>
+    <hr class="!border-t-4" />
+    <div class="card">
+        <section class="p-4">Lesson 1</section>
+        <section class="p-4">Lesson 2</section>
+        <section class="p-4">Lesson 3</section>
+    </div>
+</body>
+<footer class="card-footer">Thanks for your attention</footer>
